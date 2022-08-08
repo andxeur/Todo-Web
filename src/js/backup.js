9c4a0320569d87c24d74150cb3691e33ba68feaf
drex_xdev <codingzipdev@gmail.com>
2022-08-08 22:31:45 +0100

    remise en forme globale
    remise en forme globale et des fonctionnalités principale

--- src/js/backup.js
+++ src/js/backup.js
@@ -1,64 +1,119 @@
-// Selectors
-const zoneDeSaisiePrincipale = document.querySelector(".header_bottom_centre_contain_menu_gauche_form2_ZoneDeSaisie");
+// Selecteur des Zone de Saisie
+const zoneDeSaisie_tire = document.querySelector(
+  ".header_bottom_centre_contain_menu_gauche_form_Titre"
+);
+const zoneDeSaisieDescription = document.querySelector(
+  ".header_bottom_centre_contain_menu_gauche_form2_Description"
+);
+//Selecteur des  categorie
+const menuSelectionDeCategorie = document.querySelector(
+  ".header_bottom_centre_contain_menu_gauche_form_div"
+);
+const listeDesCategorie = document.querySelector(
+  ".Categorie_selectioner_liste"
+);
+
+// Selecteur des boutton
+// const selecteurCategorie = document.querySelector(".icon_Categorie");
 const btn_ajoutDuneTache = document.querySelector(".icon_add");
-const ZoneDajoutDesTache = document.querySelector(".header_bottom_centre_bottom_centrer_listeDesTaches");
+const ZoneDajoutDesTache = document.querySelector(
+  ".header_bottom_centre_bottom_centrer_listeDesTaches"
+);
 const filterOption = document.querySelector(".filter-todo");
 
-
 // Event Listener
-// document.addEventListener("DOMContentLoaded", obtenirLesTachePresente);
-btn_ajoutDuneTache.addEventListener("click", AjoutDesTache);
+document.addEventListener("DOMContentLoaded", obtenirLesTachePresente);
+btn_ajoutDuneTache.addEventListener("click", controlZoneZaisie);
 ZoneDajoutDesTache.addEventListener("click", effacerLaTacheSElectioner);
-filterOption.addEventListener("click", filtrerLesTache);
+// Event Listener de la categorie
+menuSelectionDeCategorie.addEventListener("click", afficherLesCategorie);
+// filterOption.addEventListener("click", filtrerLesTache);
+
+console.log(localStorage.getItem("tacheTodo"));
 
 // Functions
-function AjoutDesTache(event) {
-  event.preventDefault();
-
+function AjoutDesTache() {
   // creationDuneDiv div
   const creationDuneDivPourAficherLesTaches = document.createElement("div");
-  creationDuneDivPourAficherLesTaches.classList.add("stylePourLa_listeDesTaches");
-  // create li
-  const nouvelTache = document.createElement("li");
-  nouvelTache.innerText = zoneDeSaisiePrincipale.value;
-  nouvelTache.classList.add("iteme_des_tache");
-  creationDuneDivPourAficherLesTaches.appendChild(nouvelTache);
+  creationDuneDivPourAficherLesTaches.classList.add(
+    "stylePourLa_listeDesTaches"
+  );
+
+  // create div
+   const divConteneurDinfo = document.createElement("div");
+  divConteneurDinfo.classList.add("divConteneurDinfo");
+  divConteneurDinfo.innerHTML = `
+  <div class="statueTache">
+  <div class="statueTache_cercle"></div>
+  </div>
+
+  <div class="contenuDeTache">
+
+     <div class="contenuDeTache_titreEtCategorie">
+     <h4>${zoneDeSaisie_tire.value}</h4>
+     <h4>${zoneDeSaisie_tire.value}</h4>
+     </div>
+
+     <div class="contenuDeTache_description">
+     <p>${zoneDeSaisieDescription.value}</p>
+     </div>
+  </div>
+
+  <div class="editerLaTache"></div>
+
+  <div class="effacerLaTache"></div>
+  `;
+
+  creationDuneDivPourAficherLesTaches.appendChild(divConteneurDinfo);
   // sauvegarder DesTaches en Local
-  sauvegardeLocalDesTaches(zoneDeSaisiePrincipale.value);
-  // check mark button
-  const btn_tacheTerminer = document.createElement("button");
-  btn_tacheTerminer.innerHTML = '<i class="fas fa-check"></i>';
-  btn_tacheTerminer.classList.add("complete-btn");
-  creationDuneDivPourAficherLesTaches.appendChild(btn_tacheTerminer);
-  // check trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("trash-btn");
-  creationDuneDivPourAficherLesTaches.appendChild(trashButton);
-  // append to list
+  sauvegardeLocalDesTaches(
+    zoneDeSaisie_tire.value,
+    zoneDeSaisieDescription.value
+  );
+  // ajouter a la zone des tache
   ZoneDajoutDesTache.appendChild(creationDuneDivPourAficherLesTaches);
-  // clear todo input value
-  zoneDeSaisiePrincipale.value = "";
+  // effacer la valeur des input
+  zoneDeSaisieDescription.value = "";
+  zoneDeSaisie_tire.value = "";
 }
 
-// function effacerLaTacheSElectioner(e) {
-//   const item = e.target;
-//   // delete todo
-//   if (item.classList[0] === "trash-btn") {
-//     const todo = item.parentElement;
-//     // animation
-//     todo.classList.add("fall");
-//     removeLocalTodos(todo);
-//     todo.addEventListener("transitionend", function () {
-//       todo.remove();
-//     });
-//   }
-//   // check mark
-//   if (item.classList[0] === "complete-btn") {
-//     const todo = item.parentElement;
-//     todo.classList.toggle("completed");
-//   }
-// }
+function controlZoneZaisie(event) {
+  event.preventDefault();
+
+  if (zoneDeSaisie_tire.value == "") {
+    alert("le titre est vide");
+  } else {
+    AjoutDesTache();
+  }
+}
+
+function effacerLaTacheSElectioner(e) {
+  const item = e.target;
+
+  // delete todo
+  if (item.classList[0] === "effacerLaTache") {
+    // recupere le parent qui les contien
+    const item_conteneurGlobale = e.path[2];
+    // console.log(item_conteneurGlobale);
+
+    // animation
+    item_conteneurGlobale.classList.add("fall");
+
+    removeLocalTodos(item_conteneurGlobale);
+    // removeLocalTodos(e);
+
+    item_conteneurGlobale.addEventListener("transitionend", function () {
+      item_conteneurGlobale.remove();
+    });
+  }
+
+  //TODO:dd
+  // check mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
 
 // function filtrerLesTache(e) {
 //   const todos = ZoneDajoutDesTache.childNodes;
@@ -85,58 +140,106 @@ function AjoutDesTache(event) {
 //   });
 // }
 
-// function sauvegardeLocalDesTaches(todo) {
-//   // check if something exists
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
+function sauvegardeLocalDesTaches(titre, description) {
+  // localStorage.clear();
+  // console.log(localStorage.getItem("tacheTodo"));
 
-//   todos.push(todo);
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
+  // verifie si des tache exists deja
+  let listeDesTodo;
 
-// function obtenirLesTachePresente() {
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
-//   todos.forEach(function (todo) {
-//     // todo div
-//     const todoDiv = document.createElement("div");
-//     todoDiv.classList.add("todo");
-//     // create li
-//     const newTodo = document.createElement("li");
-//     newTodo.innerText = todo;
-//     newTodo.classList.add("todo-item");
-//     todoDiv.appendChild(newTodo);
-//     // check mark button
-//     const completedButton = document.createElement("button");
-//     completedButton.innerHTML = '<i class="fas fa-check"></i>';
-//     completedButton.classList.add("complete-btn");
-//     todoDiv.appendChild(completedButton);
-//     // check trash button
-//     const trashButton = document.createElement("button");
-//     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-//     trashButton.classList.add("trash-btn");
-//     todoDiv.appendChild(trashButton);
-//     // append to list
-//     ZoneDajoutDesTache.appendChild(todoDiv);
-//   });
-// }
+  const arrayInfoTodo = { titre, description };
 
-// function removeLocalTodos(todo) {
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
-//   const todoIndex = todo.children[0].innerText;
-//   todos.splice(todos.indexOf(todoIndex), 1);
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
+  if (localStorage.getItem("tacheTodo") === null) {
+    listeDesTodo = [];
+  } else {
+    listeDesTodo = JSON.parse(localStorage.getItem("tacheTodo"));
+  }
+
+  listeDesTodo.push(arrayInfoTodo);
+  localStorage.setItem("tacheTodo", JSON.stringify(listeDesTodo));
+
+  // console.log(listeDesTodo);
+  console.log(localStorage.getItem("tacheTodo"));
+}
+
+function obtenirLesTachePresente() {
+  let listeDesTacheEnregistrer;
+  if (localStorage.getItem("tacheTodo") === null) {
+    listeDesTacheEnregistrer = [];
+  } else {
+    listeDesTacheEnregistrer = JSON.parse(localStorage.getItem("tacheTodo"));
+  }
+
+  listeDesTacheEnregistrer.forEach((infoOBjet) => {
+    // console.log(infoOBjet.titre);
+    // console.log(infoOBjet.description);
+
+    // creationDuneDiv div
+    const creationDuneDivPourAficherLesTaches = document.createElement("div");
+    creationDuneDivPourAficherLesTaches.classList.add(
+      "stylePourLa_listeDesTaches"
+    );
+
+    // create div
+    const divConteneurDinfo = document.createElement("div");
+    divConteneurDinfo.classList.add("divConteneurDinfo");
+    divConteneurDinfo.innerHTML = `
+  <div class="statueTache">
+  <div class="statueTache_cercle"></div>
+  </div>
+
+  <div class="contenuDeTache">
+
+     <div class="contenuDeTache_titreEtCategorie">
+     <h4>${infoOBjet.titre}</h4>
+     <h4>${infoOBjet.titre}</h4>
+     </div>
+
+     <div class="contenuDeTache_description">
+     <p>${infoOBjet.description}</p>
+     </div>
+  </div>
+
+  <div class="editerLaTache"></div>
+
+  <div class="effacerLaTache"></div>
+  `;
+
+    creationDuneDivPourAficherLesTaches.appendChild(divConteneurDinfo);
+
+    // ajouter a la zone des tache
+    ZoneDajoutDesTache.appendChild(creationDuneDivPourAficherLesTaches);
+  });
+}
+
+function removeLocalTodos(todo) {
+  let todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("tacheTodo"));
+  }
+
+  // console.log(todo);
+  // console.log(todo.parentNode);
+  // console.log(todo.parentNode.children);
+  // console.log(todo.parentNode.children[todo.parentNode]);
+
+  //obtenire l'index de la div a supprimer
+  const index = Array.from(todo.parentElement.children).indexOf(todo);
+  // console.log("index est " + index);
+
+  todos.splice(index, 1);
+
+  localStorage.setItem("tacheTodo", JSON.stringify(todos));
+
+  console.log(localStorage.getItem("tacheTodo"));
+}
+
+
+function afficherLesCategorie() {
+  console.log('tess ok')
+
+  listeDesCategorie.classList.toggle('afficheCategorie');
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,83 @@
-// Selectors pour les Zone de Saisie
-const zoneDeSaisie_tire = document.querySelector(".header_bottom_centre_contain_menu_gauche_form_Titre");
-const zoneDeSaisieDescription = document.querySelector(".header_bottom_centre_contain_menu_gauche_form2_Description");
-// Selectors
+//-----------------------------tout le Selecteur ---------------------------------------
+
+// Selecteur des Zone de Saisie
+
+const zoneDeSaisie_tire = document.querySelector(
+  ".header_bottom_centre_contain_menu_gauche_form_Titre"
+);
+const zoneDeSaisieDescription = document.querySelector(
+  ".header_bottom_centre_contain_menu_gauche_form2_Description"
+);
+//Selecteur des  categorie
+const menuSelectionDeCategorie = document.querySelector(
+  ".Categorie_selectioner"
+);
+const listeDesCategorie = document.querySelector(
+  ".Categorie_selectioner_liste"
+);
+
+//Selecteur des categorie filter
+const menuSelectionDeCategorie_filter = document.querySelector(
+  ".Categorie_selectioner_filter"
+);
+const listeDesCategorie_filter = document.querySelector(
+  ".Categorie_selectioner_liste_filter"
+);
+const sousMenu_filter = document.querySelector(".sous_menu_filter");
+const titreSousMenu_filter = document.querySelector(
+  ".Categorie_selectioner_filter > p"
+);
+const icon_Categorie_selection_filter = document.querySelector(
+  ".Categorie_selectioner_icon_filter"
+);
+
+const sousMenu = document.querySelector(".sous_menu");
+const titreSousMenu = document.querySelector(".Categorie_selectioner > p");
+const icon_Categorie_selection = document.querySelector(
+  ".Categorie_selectioner_icon"
+);
 const btn_ajoutDuneTache = document.querySelector(".icon_add");
-const ZoneDajoutDesTache = document.querySelector(".header_bottom_centre_bottom_centrer_listeDesTaches");
-const filterOption = document.querySelector(".filter-todo");
+const ZoneDajoutDesTache = document.querySelector(
+  ".header_bottom_centre_bottom_centrer_listeDesTaches"
+);
 
-
-// Event Listener
-// document.addEventListener("DOMContentLoaded", obtenirLesTachePresente);
-btn_ajoutDuneTache.addEventListener("click", AjoutDesTache);
+// --------------------------------tout les Event Listener----------------------------------
+document.addEventListener("DOMContentLoaded", obtenirLesTachePresente);
+btn_ajoutDuneTache.addEventListener("click", controlZoneZaisie);
 ZoneDajoutDesTache.addEventListener("click", effacerLaTacheSElectioner);
-// filterOption.addEventListener("click", filtrerLesTache);
+menuSelectionDeCategorie.addEventListener("click", afficherLesCategorie);
+menuSelectionDeCategorie_filter.addEventListener(
+  "click",
+  afficherLesCategorie_filter
+);
+sousMenu_filter.addEventListener("click", (parmt) => {
+  //obtien le li du ul sur le quel on a cliquer (parmt.target;)
+  const titre_categorie = parmt.target.innerHTML;
 
-// Functions
-function AjoutDesTache(event) {
-  event.preventDefault();
+  titreSousMenu_filter.textContent = `${titre_categorie}`;
 
+  filtrerLesTache();
+});
+
+sousMenu.addEventListener("click", (parmt) => {
+  //obtien le li du ul sur le quel on a cliquer (parmt.target;)
+  const titre_categorie = parmt.target.innerHTML;
+
+  titreSousMenu.textContent = `${titre_categorie}`;
+});
+
+// ---------------------------tout les Functions---------------------------------------------
+function AjoutDesTache() {
   // creationDuneDiv div
   const creationDuneDivPourAficherLesTaches = document.createElement("div");
-  creationDuneDivPourAficherLesTaches.classList.add("stylePourLa_listeDesTaches");
+  creationDuneDivPourAficherLesTaches.classList.add(
+    "stylePourLa_listeDesTaches"
+  );
 
   // create div
   const divConteneurDinfo = document.createElement("div");
   divConteneurDinfo.classList.add("divConteneurDinfo");
-  divConteneurDinfo.innerHTML =`
+  divConteneurDinfo.innerHTML = `
   <div class="statueTache">
   <div class="statueTache_cercle"></div>
   </div>
@@ -33,7 +86,7 @@ function AjoutDesTache(event) {
 
      <div class="contenuDeTache_titreEtCategorie">
      <h4>${zoneDeSaisie_tire.value}</h4>
-     <h4>${zoneDeSaisie_tire.value}</h4>
+     <h4>${titreSousMenu.textContent}</h4>
      </div>
 
      <div class="contenuDeTache_description">
@@ -45,119 +98,181 @@ function AjoutDesTache(event) {
 
   <div class="effacerLaTache"></div>
   `;
-  
+
   creationDuneDivPourAficherLesTaches.appendChild(divConteneurDinfo);
   // sauvegarder DesTaches en Local
-  sauvegardeLocalDesTaches(zoneDeSaisieDescription.value);
-  // append to list
+  sauvegardeLocalDesTaches(
+    zoneDeSaisie_tire.value,
+    zoneDeSaisieDescription.value
+  );
+  // ajouter a la zone des tache
   ZoneDajoutDesTache.appendChild(creationDuneDivPourAficherLesTaches);
-  // clear todo input value
+  // effacer la valeur des input
   zoneDeSaisieDescription.value = "";
-  zoneDeSaisie_tire.value = "" ;
+  zoneDeSaisie_tire.value = "";
+}
+
+function controlZoneZaisie(event) {
+  event.preventDefault();
+
+  if (zoneDeSaisie_tire.value == "") {
+    alert("le titre est vide");
+  } else {
+    AjoutDesTache();
+  }
 }
 
 function effacerLaTacheSElectioner(e) {
   const item = e.target;
 
+  // recupere le parent qui les contien (class="stylePourLa_listeDesTaches")
+  const item_conteneurGlobale = e.path[2];
+
   // delete todo
   if (item.classList[0] === "effacerLaTache") {
+    // animation tombee
+    item_conteneurGlobale.classList.add("fall");
 
-    // recupere le parent qui les contien
-    const item_conteneurGlobale = e.path[2];
-    console.log(item_conteneurGlobale);
+    removeLocalTodos(item_conteneurGlobale);
+    // removeLocalTodos(e);
 
-    const todo = item_conteneurGlobale;
-    // animation
-    todo.classList.add("fall");
-    removeLocalTodos(todo);
-    todo.addEventListener("transitionend", function () {
-      todo.remove();
+    item_conteneurGlobale.addEventListener("transitionend", function () {
+      item_conteneurGlobale.remove();
     });
   }
+
   // check mark
-  if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
-    todo.classList.toggle("completed");
+  if (item.classList[0] === "statueTache_cercle") {
+    item.classList.toggle("statue_tache_completer");
+    console.log(item_conteneurGlobale);
+    console.log(item);
+    console.log(e);
+    const enveloppeurTotale = e.path[3];
+    enveloppeurTotale.classList.toggle("tache_completer");
   }
 }
 
-// function filtrerLesTache(e) {
-//   const todos = ZoneDajoutDesTache.childNodes;
-//   todos.forEach(function (todo) {
-//     switch (e.target.value) {
-//       case "all":
-//         todo.style.display = "flex";
-//         break;
-//       case "completed":
-//         if (todo.classList.contains("completed")) {
-//           todo.style.display = "flex";
-//         } else {
-//           todo.style.display = "none";
-//         }
-//         break;
-//       case "uncompleted":
-//         if (!todo.classList.contains("completed")) {
-//           todo.style.display = "flex";
-//         } else {
-//           todo.style.display = "none";
-//         }
-//         break;
-//     }
-//   });
-// }
+function sauvegardeLocalDesTaches(titre, description) {
+  // localStorage.clear();
+  // console.log(localStorage.getItem("tacheTodo"));
 
-function sauvegardeLocalDesTaches(todo) {
-  // check if something exists
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
+  // verifie si des tache exists deja
+  let listeDesTodo;
+
+  const arrayInfoTodo = { titre, description };
+
+  if (localStorage.getItem("tacheTodo") === null) {
+    listeDesTodo = [];
   } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
+    listeDesTodo = JSON.parse(localStorage.getItem("tacheTodo"));
   }
 
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
+  listeDesTodo.push(arrayInfoTodo);
+  localStorage.setItem("tacheTodo", JSON.stringify(listeDesTodo));
 }
 
 function obtenirLesTachePresente() {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
+  let listeDesTacheEnregistrer;
+  if (localStorage.getItem("tacheTodo") === null) {
+    listeDesTacheEnregistrer = [];
   } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
+    listeDesTacheEnregistrer = JSON.parse(localStorage.getItem("tacheTodo"));
   }
-  todos.forEach(function (todo) {
-    // todo div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-    // create li
-    const newTodo = document.createElement("li");
-    newTodo.innerText = todo;
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-    // check mark button
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    // check trash button
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-    // append to list
-    ZoneDajoutDesTache.appendChild(todoDiv);
+
+  listeDesTacheEnregistrer.forEach((infoOBjet) => {
+    // creationDuneDiv div
+    const creationDuneDivPourAficherLesTaches = document.createElement("div");
+    creationDuneDivPourAficherLesTaches.classList.add(
+      "stylePourLa_listeDesTaches"
+    );
+
+    // create div
+    const divConteneurDinfo = document.createElement("div");
+    divConteneurDinfo.classList.add("divConteneurDinfo");
+    divConteneurDinfo.innerHTML = `
+  <div class="statueTache">
+  <div class="statueTache_cercle"></div>
+  </div>
+
+  <div class="contenuDeTache">
+
+     <div class="contenuDeTache_titreEtCategorie">
+     <h4>${infoOBjet.titre}</h4>
+     <h4>${infoOBjet.titre}</h4>
+     </div>
+
+     <div class="contenuDeTache_description">
+     <p>${infoOBjet.description}</p>
+     </div>
+  </div>
+
+  <div class="editerLaTache"></div>
+
+  <div class="effacerLaTache"></div>
+  `;
+
+    creationDuneDivPourAficherLesTaches.appendChild(divConteneurDinfo);
+
+    // ajouter a la zone des tache
+    ZoneDajoutDesTache.appendChild(creationDuneDivPourAficherLesTaches);
+  });
+}
+
+function filtrerLesTache() {
+  const lisDesEnfantZoneDajoutDesTache = Array.from(
+    ZoneDajoutDesTache.children
+  );
+
+  lisDesEnfantZoneDajoutDesTache.forEach((enfant) => {
+    switch (titreSousMenu_filter.textContent) {
+      case "tout":
+        enfant.style.display = "flex";
+        break;
+
+      case "terminer":
+        if (enfant.classList.contains("tache_completer")) {
+          enfant.style.display = "flex";
+        } else {
+          enfant.style.display = "none";
+        }
+        break;
+
+      case "inachever":
+        if (!enfant.classList.contains("tache_completer")) {
+          enfant.style.display = "flex";
+        } else {
+          enfant.style.display = "none";
+        }
+        break;
+    }
   });
 }
 
 function removeLocalTodos(todo) {
   let todos;
+
   if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
+    todos = JSON.parse(localStorage.getItem("tacheTodo"));
   }
-  const todoIndex = todo.children[0].innerText;
-  todos.splice(todos.indexOf(todoIndex), 1);
-  localStorage.setItem("todos", JSON.stringify(todos));
+
+  //obtenire l'index de la div a supprimer
+  const index = Array.from(todo.parentElement.children).indexOf(todo);
+
+  todos.splice(index, 1);
+
+  localStorage.setItem("tacheTodo", JSON.stringify(todos));
+}
+
+function afficherLesCategorie() {
+  icon_Categorie_selection.style.transition = "1s";
+  icon_Categorie_selection.style.transform = "rotate(180deg)";
+  listeDesCategorie.classList.toggle("afficheCategorie");
+}
+
+function afficherLesCategorie_filter() {
+  icon_Categorie_selection_filter.style.transition = "1s";
+  icon_Categorie_selection_filter.style.transform = "rotate(180deg)";
+  listeDesCategorie_filter.classList.toggle("afficheCategorie_filter");
 }
